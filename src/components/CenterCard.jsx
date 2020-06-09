57bab24641356f43f8556700f7002d56642397bd
Yeswanth <swamiyeswanth@gmail.com>
2020-06-09 18:29:29 +0530

    Fixed css for mobile

--- src/components/CenterCard.jsx
+++ src/components/CenterCard.jsx
@@ -16,7 +16,7 @@ export const CenterCard = props => {
             {"Select your OTT Channel you subscribed to"}
           </h1>
           <select
-            class="self-center my-2 block appearance-none w-1/3 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            class="self-center my-2 block appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-state"
             onChange={e => {
               setOTT(e.target.value)
